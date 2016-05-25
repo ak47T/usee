75e116bbde38ac4d1431b5a59e98983de17c3d3b
ak47T <tanglubin1989@126.com>
2016-05-25 19:08:52 +0800

    first day second update

--- www/test.py
+++ www/test.py
@@ -1,26 +1,27 @@
-import sys
+# import sys
 import orm
 import asyncio
 from models import User, Blog, Comment
 
 
 def test(loop):
-    yield from orm.create_pool(user='www-data', password='www-data', db='awesome',loop=loop)
+    yield from orm.create_pool(loop=loop,user='www-data', password='www-data', db='awesome')
 
-    u = User(id='123',name='Test2', email='test2@example.com', passwd='1253', image='blank')
+    u = User(name='Test55', email='test78@example.com', passwd='1253', image='blank')
 
     yield from u.save()
 
-	
-# loop = asyncio.get_event_loop()
-# loop.run_until_complete(test())
-# loop.close()
-if __name__ == '__main__':
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete( asyncio.wait([test( loop )]) )  
-    loop.close()
-    if loop.is_closed():
-        sys.exit(0)
+	
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
+# if __name__ == '__main__':
+
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete( asyncio.wait([test( loop )]) )  
+    # loop.close()
+    # if loop.is_closed():
+    #     sys.exit(0)
 # for x in test():
 #     pass
